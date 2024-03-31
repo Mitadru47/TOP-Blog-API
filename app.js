d11a48c17160fa80c08d14e018698606967b91d7
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-03-31 20:38:02 +0530

    View Engine & DB Connection Setup

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 
+// M - Model - MVC Architecture
+// MongoDB Connection Setup
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const connectionString = "mongodb+srv://Admin-Mitadru:DB1234@clustermg.e4fjgoy.mongodb.net/blogDB?retryWrites=true&w=majority&appName=ClusterMG";
+
+async function main(){
+    await mongoose.connect(connectionString);
+}
+
+main().catch((error) => console.log(error));
+
+// V - View - MVC Architecture
+// View Engine Setup
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
+
+// C - Controllers - MVC Architecture
+// Routing
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 module.exports = app;
