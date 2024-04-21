e10c0cfdeb94aa78064b6c0c8e8acd4dbb33512e
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-04-21 10:45:19 +0530

    UserDetail API Endpoint Implementation

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,10 +2,10 @@ const asyncHandler =require("express-async-handler");
 
 // Comment Detail
 exports.comment_detail = asyncHandler(async (req, res, next) => {
-    res.send("Comment Detail - NOT IMPLEMENTED - " + req.params.postid + "/" + req.params.commentid);
+    res.status(200).json("Comment Detail - NOT IMPLEMENTED - " + req.params.postid + "/" + req.params.commentid);
 });
 
 // Comment Create POST
 exports.comment_create_post = asyncHandler(async (req, res, next) => {
-    res.send("Create Comment POST - NOT IMPLEMENTED");
+    res.status(200).json("Create Comment POST - NOT IMPLEMENTED");
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const asyncHandler = require("express-async-handler");
 
+const User = require("../models/user");
+
 // User Detail
 exports.user_detail = asyncHandler(async (req, res, next) => {
-    res.send("User Detail - NOT IMPLEMENTED - " + req.params.id);
+
+    const users = await User.find().exec();
+    res.status(200).json(users);
 });
