156fa3e67c3507b3155ad2f590bb7b6ff70e80fe
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-04-27 21:59:46 +0530

    Comment POST Endpoint Implementation

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 const Comment = require("../models/comment");
 
@@ -10,6 +11,33 @@ exports.comment_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Comment Create POST
-exports.comment_create_post = asyncHandler(async (req, res, next) => {
-    res.status(200).json("Create Comment POST - NOT IMPLEMENTED");
-});
+exports.comment_create_post = [
+
+    body("body", "Body cannot be empty!").trim().isLength({ min: 1 }).escape(),
+    body("username", "Username cannot be empty!").trim().isLength({ min: 1 }).escape(),
+    body("email", "Email cannot be empty!").trim().isLength({ min: 1 }).escape(),
+
+    asyncHandler(async (req, res, next) => {
+    
+        const error = validationResult(req);
+        const comment = new Comment({
+
+            body: req.body.body,
+
+            username: req.body.username,
+            email: req.body.email,
+
+            post: req.body.post,
+            timestamp: new Date()
+        });
+        
+        if(!error.isEmpty)
+            res.status(400).json("DB Injection Failed!");
+            
+        else {
+         
+            await comment.save();
+            res.status(200).json("Comment Added Successfully!");
+        }
+    })
+];
