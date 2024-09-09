367379bd12183c7f08411e4dd629bf02c7802897
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-09-09 12:19:36 +0530

    Added 4 Additional Samples in populateDB.js

--- populateDB.js
+++ populateDB.js
@@ -56,6 +56,10 @@ async function createPosts(){
         createPost(1, "The Fall", "The Game Aint Over… I Aint Played My Final Move", new Date(), false),
         createPost(2, "The Doubts", "I Will Keep Trying, And You will Keep Doubting Me, And We will Keep Failing", new Date(), true),
         createPost(3, "The Survivors Of Guarma", "I Am Just Trying To Make Sure Some Of Us Survive", new Date(), true),
+        createPost(4, "The Consequences", "You can’t live a bad life and have good things happen to you", new Date(), true),
+        createPost(5, "The Duality", "Just Do One Thing Or The Other, Don’t Try To Be Two People At Once…", new Date(), false),
+        createPost(6, "The Fool", "Vengeance Is An Idiot’s Game", new Date(), true),
+        createPost(7, "The Tragedy", "Lack Of Something To Feel Important About Is Almost The Greatest Tragedy A Man May Have.", new Date(), true)
     ]);
 } 
 
@@ -88,6 +92,8 @@ async function createComments(){
         createComment("genericPaolo", "genericPaolo@dummy.user", "HAVE SOME GODDAMN FAITH!!!", posts[2], new Date()),
         createComment("genericPavel", "genericPavel@dummy.user", "Breaks your heart man..", posts[0], new Date()),
         createComment("genericPascal", "genericPascal@dummy.user", "Yeah right (its sarcasm you dummies)", posts[3], new Date()),
+        createComment("genericPete", "genericPete@dummy.user", "What a quote!", posts[4], new Date()),
+        createComment("genericPeter", "genericPeter@dummy.user", "Pretty much sums it all up", posts[4], new Date())
     ]);
 }
 
